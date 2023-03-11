10f3f55b183fbb86f929e198c8bfc251bf88b3f6
Perla-Al-Haddad <perlaalhaddad@gmail.com>
2023-03-11 19:54:56 +0200

    added background image and custom html to aquarium webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,126 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path = require('path');
 import * as vscode from 'vscode';
+
+function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions & vscode.WebviewPanelOptions {
+	return {
+		enableScripts: true,
+		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')],
+	};
+}
+
+
+interface IAquariumPanel {
+	update(): void;
+}
+
+class AquariumWebviewContainer implements IAquariumPanel {
+	protected _extensionUri: vscode.Uri;
+	protected _disposables: vscode.Disposable[] = [];
+
+	constructor(
+		extensionUri: vscode.Uri,
+	) {
+		this._extensionUri = extensionUri;
+	}
+
+	protected getWebview(): vscode.Webview {
+		throw new Error('Not implemented');
+	}
+
+	protected _update() {
+		const webview = this.getWebview();
+		webview.html = this._getHtmlForWebview(webview);
+	}
+
+	public update() { }
+
+	protected _getHtmlForWebview(webview: vscode.Webview) {
+		const stylesPathMainPath = vscode.Uri.joinPath(
+			this._extensionUri,
+			'media',
+			'style.css',
+		);
+		const backgroundPathMainPath = vscode.Uri.joinPath(
+			this._extensionUri,
+			'media',
+			'background.png',
+		);
+
+		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+		const backgroundImageUri = webview.asWebviewUri(backgroundPathMainPath);
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${stylesMainUri}" rel="stylesheet">
+				<style>
+					.aquarium-view {
+						background-image: url('${backgroundImageUri}');
+					}
+				</style>
+				<title>VS Code Aquarium</title>
+			</head>
+			<body>
+				<div class='aquarium-view'>
+				</div>
+			</body>
+			</html>
+		`;
+	}
+}
+
+class AquariumWebviewViewProvider extends AquariumWebviewContainer {
+	public static readonly viewType = 'aquariumView';
+
+	private _webviewView?: vscode.WebviewView;
+
+	resolveWebviewView(webviewView: vscode.WebviewView): void | Thenable<void> {
+		this._webviewView = webviewView;
+
+		webviewView.webview.options = getWebviewOptions(this._extensionUri);
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+	}
+
+	update() {
+		this._update();
+	}
+
+	getWebview(): vscode.Webview {
+		if (this._webviewView === undefined) {
+			throw new Error(
+				vscode.l10n.t(
+					'Panel not active, make sure the aquarium view is visible before running this command.',
+				),
+			);
+		} else {
+			return this._webviewView.webview;
+		}
+	}
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-code-aquarium" is now active!');
+	let webviewViewProvider = new AquariumWebviewViewProvider(
+        context.extensionUri,
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-code-aquarium.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-code-aquarium!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+            AquariumWebviewViewProvider.viewType,
+            webviewViewProvider,
+        ),
+    );
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
